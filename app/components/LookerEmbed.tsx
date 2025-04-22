51b2a5f0d1c2081453ba0951cdc627bb995fce27
ShwetaBhattacharjee <shwetabhattacharje@gmail.com>
2025-04-22 13:17:02 +0400

    ok

--- app/components/LookerEmbed.tsx
+++ app/components/LookerEmbed.tsx
@@ -1,8 +1,8 @@
 const LookerEmbed = () => {
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen h-screen p-6">
       <iframe
-        className="w-full h-full"
+        className="w-full h-full rounded-lg shadow-lg"
         src="https://lookerstudio.google.com/embed/reporting/88bd1cc6-e5b7-4cb0-b662-fad0684f6eb7/page/p_invm8hnsvc"
         frameBorder="0"
         allowFullScreen
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,11 @@
 export default function ContactPage() {
   return (
     <main className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+      <h1 className="text-3xl font-bold mb-4">Get in Touch with Us</h1>
       <p>
-        If you have any questions or inquiries, feel free to reach out AI Unit!
+        We’re always eager to hear from you. Whether you have questions,
+        feedback, or just want to connect, the AI Unit is here and ready to
+        assist you. Don’t hesitate to reach out—we’re just a message away!
       </p>
     </main>
   );
