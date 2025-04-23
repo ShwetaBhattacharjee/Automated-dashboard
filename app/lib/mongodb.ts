0ef21b9494b6fc16319b36067dc1322bb9c1a3cf
ShwetaBhattacharjee <shwetabhattacharje@gmail.com>
2025-04-23 19:59:00 +0400

    loader

--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -4,14 +4,14 @@ import { MongoClient } from "mongodb";
 const uri = "mongodb+srv://a3:rouge1234@cluster0.mjpaoju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const options = {};
 
-let client: MongoClient;
+const client: MongoClient;
 
 declare global {
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
+  const _mongoClientPromise: Promise<MongoClient> | undefined;  // Replaced `let` with `const`
 }
 
 if (!global._mongoClientPromise) {
-  client = new MongoClient(uri, options);
+  const client = new MongoClient(uri, options);
   global._mongoClientPromise = client.connect();
 }
 
